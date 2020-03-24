871346ba363f4bac5e65204cd04f8baf202ac273
Michael <mmbmf.mm@gmail.com>
2020-03-24 15:38:14 -0500

    seed data

---
+++ seeds/seed.rideboost_tables.sql
@@ -0,0 +1,16 @@
+INSERT INTO
+    rideboost_users (
+        first_name,
+        last_name,
+        user_email,
+        password,
+        zip_code
+    )
+VALUES
+    (
+        'Kaela',
+        'Salmond',
+        'ksalmond0@tripod.com',
+        '$2a$12$jZ/bmvRs6fJ6seI7eF7AnOt1LCFtnn7Q33qYFd2aZaUJnb4ouFw06',
+        '64131'
+    );
