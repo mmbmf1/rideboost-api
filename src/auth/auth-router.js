a3cdde680e3e59d2daee08cd123535e9d050d660
Michael <mmbmf.mm@gmail.com>
2020-03-24 18:37:20 -0500

    updated user login endpoint

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -5,16 +5,16 @@ const authRouter = express.Router();
 const jsonBodyParser = express.json();
 
 authRouter.post("/login", jsonBodyParser, (req, res, next) => {
-  const { user_name, password } = req.body;
-  const loginUser = { user_name, password };
-  console.log(loginUser);
+  console.log(req.body);
+  const { user_email, password } = req.body;
+  const loginUser = { user_email, password };
 
   for (const [key, value] of Object.entries(loginUser))
     if (value == null)
       return res.status(400).json({
         error: `Missing '${key}' in request body`
       });
-  AuthService.getUserWithUserName(req.app.get("db"), loginUser.user_name)
+  AuthService.getUserWithUserEmail(req.app.get("db"), loginUser.user_email)
     .then(dbUser => {
       if (!dbUser)
         return res.status(400).json({
@@ -30,7 +30,7 @@ authRouter.post("/login", jsonBodyParser, (req, res, next) => {
             error: `Incorrect User Name or Password`
           });
 
-        const sub = dbUser.user_name;
+        const sub = dbUser.user_email;
         const payload = { user_id: dbUser.id };
 
         res.send({
--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -3,9 +3,9 @@ const jwt = require("jsonwebtoken");
 const config = require("../config");
 
 const AuthService = {
-  getUserWithUserName(db, user_name) {
+  getUserWithUserEmail(db, user_email) {
     return db("rideboost_users")
-      .where({ user_name })
+      .where({ user_email })
       .first();
   },
 
