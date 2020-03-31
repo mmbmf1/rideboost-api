bfe9ec61daa93a26af190bfc86c7493b00848f15
Michael <mmbmf.mm@gmail.com>
2020-03-31 13:51:46 -0500

    airport store

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -3,6 +3,7 @@ const unirest = require("unirest");
 const moment_timezone = require("moment-timezone");
 const zipcodes = require("zipcodes");
 const zipcode_to_timezone = require("zipcode-to-timezone");
+// const airports = require("airport-codes");
 const UsersService = require("./users-service");
 const config = require("../config");
 
@@ -14,7 +15,6 @@ usersRouter.get("/dashboard/:user_id", jsonBodyParser, (req, res, next) => {
     .then(zipcode => {
       const zip_code = zipcode[0].zip_code;
       const timezone = zipcode_to_timezone.lookup(zip_code);
-      // console.log(timezone);
       const currentDate = moment_timezone()
         .tz(timezone)
         .format("YYYY-MM-DDTHH:MM");
@@ -24,6 +24,12 @@ usersRouter.get("/dashboard/:user_id", jsonBodyParser, (req, res, next) => {
         .format("YYYY-MM-DDTHH:MM");
 
       const location = zipcodes.lookup(zip_code);
+
+      // console.log(location);
+
+      // const airport = airports.findWhere({ city: location.city }).get('icao');
+
+      // console.log(airport);
 
       const currentWeatherPromise = new Promise((resolve, reject) => {
         unirest(
@@ -51,10 +57,10 @@ usersRouter.get("/dashboard/:user_id", jsonBodyParser, (req, res, next) => {
         });
       });
 
-      const arrivalPromise = new Promise((resolve, reject) => {
+      const airlinePromise = new Promise((resolve, reject) => {
         unirest(
           "GET",
-          `https://aerodatabox.p.rapidapi.com/flights/airports/icao/KMCI/${currentDate}/${futureDate}?withLeg=false&direction=Arrival`
+          `https://aerodatabox.p.rapidapi.com/flights/airports/icao/KMCI/${currentDate}/${futureDate}?withLeg=false`
         )
           .headers({
             "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
@@ -69,28 +75,28 @@ usersRouter.get("/dashboard/:user_id", jsonBodyParser, (req, res, next) => {
           });
       });
 
-      const departuresPromise = new Promise((resolve, reject) => {
-        unirest(
-          "GET",
-          `https://aerodatabox.p.rapidapi.com/flights/airports/icao/KMCI/${currentDate}/${futureDate}?withLeg=false&direction=Departure`
-        )
-          .headers({
-            "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
-            "x-rapidapi-key": `${config.AIRLINE_KEY}`
-          })
-          .end(res => {
-            if (res.error) {
-              reject(res.error);
-            } else {
-              resolve(JSON.parse(res.raw_body));
-            }
-          });
-      });
+      // const departuresPromise = new Promise((resolve, reject) => {
+      //   unirest(
+      //     "GET",
+      //     `https://aerodatabox.p.rapidapi.com/flights/airports/icao/KMCI/${currentDate}/${futureDate}?withLeg=false&direction=Departure`
+      //   )
+      //     .headers({
+      //       "x-rapidapi-host": "aerodatabox.p.rapidapi.com",
+      //       "x-rapidapi-key": `${config.AIRLINE_KEY}`
+      //     })
+      //     .end(res => {
+      //       if (res.error) {
+      //         reject(res.error);
+      //       } else {
+      //         resolve(JSON.parse(res.raw_body));
+      //       }
+      //     });
+      // });
 
       const eventPromise = new Promise((resolve, reject) => {
         unirest(
           "GET",
-          `http://api.eventful.com/json/events/search?app_key=${config.EVENT_KEY}&location=${zip_code}&date=today&within=25&category=music, sports&sort_order=popularity`
+          `http://api.eventful.com/json/events/search?app_key=${config.EVENT_KEY}&location=${zip_code}&date=Today&within=15&category=music, sports&sort_order=date&mature=safe`
         ).end(res => {
           if (res.error) {
             reject(res.error);
@@ -103,8 +109,7 @@ usersRouter.get("/dashboard/:user_id", jsonBodyParser, (req, res, next) => {
       Promise.all([
         currentWeatherPromise,
         forecastWeatherPromise,
-        arrivalPromise,
-        departuresPromise,
+        airlinePromise,
         eventPromise,
         location
       ]).then(data => res.json({ data }));
